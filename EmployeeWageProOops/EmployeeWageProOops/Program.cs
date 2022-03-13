d650bd450397b47c0bf3a7d1ade84fd06c531424
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-13 13:13:51 +0530

    check Emp present or absent

---
+++ EmployeeWageProOops/EmployeeWageProOops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmpWageOops EmployeeWage = new EmpWageOops();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
+        }
+    }
+}
