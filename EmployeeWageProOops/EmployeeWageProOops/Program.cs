bce5848fbfa22b69d3c27bb3edb4fab1579d606e
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-15 17:42:44 +0530

    EmpWageForMultipeCompanies

--- EmployeeWageProOops/EmployeeWageProOops/Program.cs
+++ EmployeeWageProOops/EmployeeWageProOops/Program.cs
@@ -12,8 +12,12 @@ namespace EmployeeWageProOops
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             EmpWageOops EmployeeWage = new EmpWageOops();
-            EmployeeWage.Attendance();
-           
+            EmployeeWage.Attendance("DMart", 20, 2, 10);
+            EmployeeWage.Attendance("BigBazar", 10, 4, 20);
+            EmployeeWage.Attendance("BigBascket", 15, 6, 22);
+
+            Console.ReadKey();
+
         }
     }
 }
